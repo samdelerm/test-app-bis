b2aa2e3a48b5d40f73ab0348989b5420570a425c
samdelerm <samuel.legrand33840@gmail.com>
2025-03-04 16:16:20 +0100

    test

---
+++ from flask import Flask, render_template.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, jsonify, request
+import requests
+import logging
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # Add this line to enable CORS
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/get_matches')
+def get_matches():
+    try:
+        response = requests.get("https://app-rsk.onrender.com/get_matches")
+        response.raise_for_status()
+        matches = response.json()
+        return jsonify(matches)
+    except requests.exceptions.RequestException as e:
+        logging.error(f"Erreur lors de la récupération des matchs: {e}")
+        return jsonify({"error": "Erreur lors de la récupération des matchs"}), 500
+
+@app.route('/get_team_info')
+def get_team_info():
+    match_id = request.args.get('match_id')
+    try:
+        response = requests.get(f"https://app-rsk.onrender.com/get_team_info?match_id={match_id}")
+        response.raise_for_status()
+        team_info = response.json()
+        return jsonify(team_info)
+    except requests.exceptions.RequestException as e:
+        logging.error(f"Erreur lors de la récupération des informations de l'équipe: {e}")
+        return jsonify({"error": "Erreur lors de la récupération des informations de l'équipe"}), 500
+
+if __name__ == "__main__":
+    app.run(debug=True)
